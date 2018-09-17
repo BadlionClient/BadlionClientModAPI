2410f590c9dbc76ee2de5716f951ec4a602c0c6d
lukas81298 <lukas81298@gmail.com>
2018-09-17 02:41:14 +0200

    fixed compatibility for 1.13+ clients

--- blcmodapibungee/src/main/java/net/badlion/blcmodapibungee/listener/PlayerListener.java
+++ blcmodapibungee/src/main/java/net/badlion/blcmodapibungee/listener/PlayerListener.java
@@ -5,6 +5,7 @@ import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.protocol.ProtocolConstants;
 import net.md_5.bungee.protocol.packet.PluginMessage;
 
 public class PlayerListener implements Listener {
@@ -20,7 +21,8 @@ public class PlayerListener implements Listener {
 	public void onLogin(PostLoginEvent event) {
 		// Send the disallowed mods to players when they login to the proxy. A notification will appear on the Badlion Client so they know the mod was disabled
 		ProxiedPlayer player = event.getPlayer();
-		player.unsafe().sendPacket(new PluginMessage("BLC|M", BlcModApiBungee.GSON_NON_PRETTY.toJson(this.plugin.getConf().getModsDisallowed()).getBytes(), false));
+		if ( player.getPendingConnection().getVersion() <= ProtocolConstants.MINECRAFT_1_12_2 ) { // do not send to 1.13+ clients, this would break the connection
+			player.unsafe().sendPacket( new PluginMessage( "BLC|M", BlcModApiBungee.GSON_NON_PRETTY.toJson( this.plugin.getConf().getModsDisallowed() ).getBytes(), false ) );
+		}
 	}
-
 }
