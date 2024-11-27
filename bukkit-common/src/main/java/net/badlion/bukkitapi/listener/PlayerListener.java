ec3dad1d428822f0c4c218cd1ceb20bcda1f6851
xosteel <xosteel@protonmail.com>
2024-11-27 22:37:54 +0100

    Add breeding timer support + missing survival features

--- bukkit-common/src/main/java/net/badlion/bukkitapi/listener/PlayerListener.java
+++ bukkit-common/src/main/java/net/badlion/bukkitapi/listener/PlayerListener.java
@@ -1,14 +1,19 @@
 package net.badlion.bukkitapi.listener;
 
 import net.badlion.bukkitapi.AbstractBukkitBadlionPlugin;
+import net.badlion.bukkitapi.survival.BreedingTracker;
 import net.badlion.bukkitapi.timers.TimerApi;
 import net.badlion.modapicommon.AbstractBadlionApi;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Ageable;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityBreedEvent;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
@@ -19,9 +24,11 @@ import java.nio.charset.StandardCharsets;
 public class PlayerListener implements Listener {
 
 	private final AbstractBukkitBadlionPlugin plugin;
+	private final BreedingTracker breedingTracker;
 
 	public PlayerListener(AbstractBukkitBadlionPlugin plugin) {
 		this.plugin = plugin;
+		this.breedingTracker = new BreedingTracker(plugin);
 	}
 
 	@EventHandler
@@ -41,6 +48,7 @@ public class PlayerListener implements Listener {
 		this.plugin.getSurvivalManager().sendData(player);
 
 		Bukkit.getScheduler().runTaskLater(this.plugin, () -> this.plugin.getWaypointManager().sendWaypointsToClient(player, player.getWorld()), 40);
+		this.breedingTracker.trackAll(event.getPlayer());
 	}
 
 	@EventHandler
@@ -67,5 +75,25 @@ public class PlayerListener implements Listener {
 	@EventHandler
 	public void onJoinWorld(PlayerChangedWorldEvent event) {
 		this.plugin.getSurvivalManager().sendData(event.getPlayer());
+		this.breedingTracker.trackAll(event.getPlayer());
+	}
+
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+	public void onBreed(EntityBreedEvent event) {
+		if (!(event.getBreeder() instanceof Player)) {
+			return;
+		}
+
+		Bukkit.getScheduler().scheduleSyncDelayedTask(this.plugin, () ->
+				this.breedingTracker.track((Player) event.getBreeder(), false, event.getMother(), event.getFather(), event.getEntity()), 1);
+	}
+
+	@EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+	public void onInteract(PlayerInteractEntityEvent event) {
+		Entity entity = event.getRightClicked();
+
+		if (entity instanceof Ageable && ((Ageable) entity).getAge() != 0) {
+			this.breedingTracker.track(event.getPlayer(), true, entity);
+		}
 	}
 }
